12776b8457ed8394cf8bd8311ea4601e65eea5ff
Sudarshan singh <sudarshans471@gmail.com>
2023-04-20 19:25:48 +1000

    first commit

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,31 @@
+import About from "./components/About";
+import Contact from "./components/contact";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
+import SocialLinks from "./components/SocialLinks";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    
+      <Home />
+
+      <About  />
+      
+      <Portfolio  />
+    
+      <Experience  />
+    
+      <Contact />
+
+      <SocialLinks />
+
+      
+    </div>
+  );
+}
+
+export default App;
